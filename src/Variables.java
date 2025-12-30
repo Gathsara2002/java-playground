4ef01cf50cb30b61008114852c819c000af625ba
Gathsara2002 <liviru2002mmg@gmail.com>
2025-12-30 11:59:58 +0530

    java variables

---
+++ src/Variables.java
@@ -0,0 +1,22 @@
+/**
+ * @author : Gathsara
+ * created : 12/30/2025 -- 11:08 AM
+ **/
+
+public class Variables {
+    public static void main(String[] args) {
+
+        //Variable - variable is container for storing values temporarily.
+
+        //Two type of java variables
+        //1. Primitive - hold primitive data in stack(memory) (byte, short, int, float, double, long, chart, boolean)
+        //2. Reference  - memory address point the heap (String, Array, Object)
+
+        int age = 23;
+        double gpa = 3.9;
+        char grade = 'A';
+        boolean isOnline = true;
+        String name = "John";
+
+    }
+}
