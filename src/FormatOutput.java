60b5ad171cbe24971aff0c506ca774fcbf290f35
Gathsara2002 <liviru2002mmg@gmail.com>
2025-12-30 17:43:45 +0530

    formatted output

---
+++ src/FormatOutput.java
@@ -0,0 +1,25 @@
+/**
+ * @author : Gathsara
+ * created : 12/30/2025 -- 5:21 PM
+ **/
+
+public class FormatOutput {
+    public static void main(String[] args) {
+
+        //printf() - used to format output
+
+        String name = "John";
+        char firstLetter = 'J';
+        int age = 20;
+        double height = 181.5;
+        boolean isStudent = true;
+
+        System.out.printf("Your name is %s\n", name);
+        System.out.printf("Your name start with %c\n", firstLetter);
+        System.out.printf("Your age is  %d\n", age);
+        System.out.printf("Your height is  %.1f\n", height);
+        System.out.printf("Your are student : %b\n", isStudent);
+
+        System.out.printf("%s is %d year old\n", name, age);
+    }
+}
