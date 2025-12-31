268c614ce241e70673b28aaf0cb935acc47f3df5
Gathsara2002 <liviru2002mmg@gmail.com>
2025-12-31 11:02:28 +0530

    java string methods

---
+++ src/StringMethods.java
@@ -0,0 +1,23 @@
+/**
+ * @author : Gathsara
+ * created : 12/31/2025 -- 10:52 AM
+ **/
+
+public class StringMethods {
+    public static void main(String[] args) {
+
+        //String - string is a sequence of characters surrounded by double quotes ("")
+
+        String name= "John Doe";
+
+        int length = name.length();
+        System.out.println(length);
+
+        char letter = name.charAt(4);
+        System.out.println(letter);
+
+        int index = name.indexOf(" ");
+        System.out.println(index);
+
+    }
+}
