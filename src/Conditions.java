26b18b4e542d6281e043c46d58c1fd658731b911
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-06 10:54:55 +0530

    ternary operator

--- src/Conditions.java
+++ src/Conditions.java
@@ -26,5 +26,23 @@ public class Conditions {
         } else {
             System.out.println("Your are a child");
         }
+
+        //ternary operator
+        //Variable  = (Condition) ? if true : if false
+
+        int marks = 46;
+        if (marks >= 35) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+        String result = (marks >= 35) ? "Pass" : "Fail";
+        System.out.println(result);
+
+        System.out.println((marks >= 35) ? "Pass" : "Fail");
+
+        result = (marks <= 35) ? "Fail" : (marks > 35 && marks <= 50) ? "S" : (marks > 50 && marks <= 75) ? "B" : "A";
+        System.out.println("Result : " + result);
     }
 }
