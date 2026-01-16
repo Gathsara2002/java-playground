3a910611b86718f26b5b822ee8a7676532f412c7
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-16 17:10:16 +0530

    methods

---
+++ src/Methods.java
@@ -0,0 +1,42 @@
+/**
+ * @author : Gathsara
+ * created : 1/16/2026 -- 4:34 PM
+ **/
+
+public class Methods {
+    public static void main(String[] args) {
+
+        //Methods - Reusable block of code which only run when it's called
+        //Cannot define method inside another method because java doesn't support nested methods
+        //Method signature - method name + parameters
+
+        subMethod1();
+        subMethod2("Hello");
+        System.out.println(subMethod3());
+        System.out.println(subMethod4("Hi"));
+        System.out.println(subMethod4(1));
+    }
+
+    static void subMethod1() {
+        System.out.println("This is method do not return value and do not accept argument");
+    }
+
+    static void subMethod2(String param) {
+        System.out.println(param + " :This is method do not return value but accept one argument");
+    }
+
+    static String subMethod3() {
+        return "This is method  return value and do not accept argument";
+    }
+
+    static String subMethod4(String param) {
+        return param+ " :This is method  return value and accept one argument";
+    }
+
+    static String subMethod4(int param) {
+        return param+ " : method overloading";
+    }
+
+
+
+}
