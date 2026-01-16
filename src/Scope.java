1bb8a1ade88779d773d490c6aa875eeb78c4dcea
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-16 17:40:01 +0530

    variable scope

---
+++ src/Scope.java
@@ -0,0 +1,31 @@
+/**
+ * @author : Gathsara
+ * created : 1/16/2026 -- 5:14 PM
+ **/
+
+public class Scope {
+
+    static int y = 3; //Class - declare inside class
+
+    public static void main(String[] args) {
+
+        // variable scope - where can variable access
+
+        //1.Local - declare inside method
+        int x = 1;
+        System.out.println(x);
+        System.out.println(y);
+
+        sample();
+
+    }
+
+    static void sample() {
+        int x = 2;  //Local
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+    //Local variables can be used inside the method it declares
+    //Class variables can be used inside anywhere of the declared class
+}
