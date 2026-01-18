5a5d254d55b4ce546f5f1cd4ab73437fd318a7b7
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-18 19:31:24 +0530

    inheritance

---
+++ src/oop/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package oop.inheritance;
+
+/**
+ * @author : Gathsara
+ * created : 1/18/2026 -- 7:20 PM
+ **/
+
+public class Animal extends Organism{
+
+    void eat(){
+        System.out.println("Animal eats");
+    }
+}
---
+++ src/oop/inheritance/Organism.java
@@ -0,0 +1,15 @@
+package oop.inheritance;
+
+/**
+ * @author : Gathsara
+ * created : 1/18/2026 -- 7:26 PM
+ **/
+
+public class Organism {
+
+    boolean isAlive;
+
+    Organism() {
+        isAlive = true;
+    }
+}
