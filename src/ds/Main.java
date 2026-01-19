75c6f66db393b66e0285135fc2faa0e6caa1dc83
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-19 16:08:33 +0530

    ArrayList

---
+++ src/ds/Main.java
@@ -0,0 +1,32 @@
+package ds;
+
+import java.util.ArrayList;
+
+/**
+ * @author : Gathsara
+ * created : 1/19/2026 -- 3:53 PM
+ **/
+
+public class Main {
+    public static void main(String[] args) {
+
+        //ArrayList - resizable array that store objects
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //add objects
+        list.add(1);
+        list.add(3);
+        list.add(7);
+        list.add(11);
+        System.out.println(list);  //In here display the elements instead of memory address because ArrayList has customized toString() instead of default.
+
+        //remove element
+        list.remove(2);
+        System.out.println(list);
+
+        //get element
+        System.out.println(list.get(1));
+
+    }
+}
