c1baae0c3bc668f5042276a55da133556481439c
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-19 12:48:08 +0530

    polymorphism

---
+++ src/oop/polymorphism/Bike.java
@@ -0,0 +1,13 @@
+package oop.polymorphism;
+
+/**
+ * @author : Gathsara
+ * created : 1/19/2026 -- 12:43 PM
+ **/
+
+public class Bike extends Vehicle{
+    @Override
+    void go() {
+        System.out.println("You drive bike");
+    }
+}
