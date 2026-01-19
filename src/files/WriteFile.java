004815652ef3b647b9b82f7aa56582bd0410cc1d
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-19 16:31:10 +0530

    write file

---
+++ src/files/WriteFile.java
@@ -0,0 +1,34 @@
+package files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author : Gathsara
+ * created : 1/19/2026 -- 4:22 PM
+ **/
+
+public class WriteFile {
+    public static void main(String[] args) {
+
+        //4 options to write file
+
+        //FileWriter - good for small and medium-sized text files
+        //BufferWriter - better performance for large amount of texts
+        //PrintWriter - best for structured files (report)
+        //FileOutputStream - best for binary files (images)
+
+        String path = "E:\\Playground\\Java\\Playground\\src\\files\\";
+        String message = """
+                This is sample text.
+                Multiple line message.
+                """;
+
+        try (FileWriter fileWriter = new FileWriter(path+"filename.txt")) {
+            fileWriter.write(message);
+            System.out.println("File has been written");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
