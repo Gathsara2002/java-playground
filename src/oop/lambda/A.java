017205f3ec69abb753b5ce084437a5fc180182e3
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-22 15:02:41 +0530

    lambda expression

---
+++ src/oop/lambda/A.java
@@ -0,0 +1,10 @@
+package oop.lambda;
+
+/**
+ * @author : Gathsara
+ * created : 1/22/2026 -- 2:45 PM
+ **/
+
+public interface A {
+    void show();
+}
---
+++ src/oop/lambda/C.java
@@ -0,0 +1,10 @@
+package oop.lambda;
+
+/**
+ * @author : Gathsara
+ * created : 1/22/2026 -- 2:53 PM
+ **/
+
+public interface C {
+    int sum(int a, int b);
+}
