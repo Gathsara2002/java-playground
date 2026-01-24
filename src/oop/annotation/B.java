e8fb3f81fccd5dab9429389c6d6108364c9940c6
Gathsara2002 <liviru2002mmg@gmail.com>
2026-01-24 16:15:50 +0530

    annotation

---
+++ src/oop/annotation/B.java
@@ -0,0 +1,13 @@
+package oop.annotation;
+
+/**
+ * @author : Gathsara
+ * created : 1/24/2026 -- 4:09 PM
+ **/
+
+public class B extends A {
+    @Override
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("This is B");
+    }
+}
